adcce849879a555db72b79e61cec284273bdcd0d
PROGRAMISTO <habib2004habib@yandex.ru>
2025-04-11 14:52:08 +0300

    Create Managers

---
+++ Assets/# Scripts/AudioManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private bool m_isSoundOn = true;
+    [SerializeField] private AudioListener playerListener;
+
+
+    public void SwitchSound()
+    {
+        m_isSoundOn = !m_isSoundOn;
+
+        playerListener.enabled = m_isSoundOn;
+    }
+}
